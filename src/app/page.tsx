9cbae6b96b8738e9d63f240793ae44d341522bd5
satyahari789 <satyabshine452@gmail.com>
2025-05-15 15:50:21 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
  import SpinWheel from '../app/UI/spinwheel'
+  
 
 export default function Home() {
   return (
      <>
      <h1>Welcome to the Home page</h1>
      <SpinWheel/>
+     
      </>
   );
 }
